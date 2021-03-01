77475a43d624f54fe89fa60c123ba6becc050130
Vinay Tallapalli <vinay.vinny18@gmail.com>
2021-03-01 01:41:45 -0500

    Reactive Var example

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,27 @@
 import styles from '../styles/Home.module.css'
-import { gql, useQuery } from '@apollo/client'
+import { gql, useQuery, useReactiveVar } from '@apollo/client'
+import { reactiveVar } from '../lib/cache'
 
 const LOCAL_ITEM = gql`
 	query getLocalOnlyItem {
 		localOnlyField @client
+		# reactiveVarField @client
 	}
 `
 
 export default function Home() {
 	const { data } = useQuery(LOCAL_ITEM)
+	const values = useReactiveVar(reactiveVar)
+
+	const updateState = (state) => {
+		reactiveVar({ ...state, signedIn: !state.signedIn })
+	}
 	console.log(data)
+	console.log(values)
 	return (
 		<div className={styles.container}>
-			{ JSON.stringify(data, null, 2) }
+			{ JSON.stringify(values, null, 2) }
+			<button onClick={() => updateState(values)}>SIGN IN</button>
 		</div>
 	)
 }
