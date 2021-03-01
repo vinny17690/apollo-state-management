650f7023227006b4c80753dcd927d789af5f5d4e
Vinay Tallapalli <vinay.vinny18@gmail.com>
2021-03-01 01:29:01 -0500

    Local Read Only field example

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
 import '../styles/globals.css'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+	uri: 'https://api.spacex.land/graphql',
+	cache: new InMemoryCache({
+		typePolicies: {
+			Query: {
+				fields: {
+					localOnlyField: {
+						read() {
+							return 'local only field value'
+						}
+					}
+				}
+			}
+		}
+	})
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<ApolloProvider client={client}>
+			<Component {...pageProps} />
+		</ApolloProvider>
+	)
 }
 
 export default MyApp
